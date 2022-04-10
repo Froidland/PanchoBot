06e673d1a5db5a510e0fd502e278504b14280c24
Francisco Hauva <phauva@gmail.com>
2022-04-09 23:10:08 -0400

    Added logging and configuration of the database handler in this class.

--- src/DiscordBot.cs
+++ src/DiscordBot.cs
@@ -3,8 +3,11 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using Microsoft.Extensions.Logging;
+using MySqlConnector.Logging;
 using PanchoBot.Api.v2;
 using PanchoBot.Commands;
+using PanchoBot.Database;
+using Serilog;
 
 namespace PanchoBot;
 
@@ -12,7 +15,12 @@ public class DiscordBot {
     private DiscordClient? DiscordClient { get; set; }
     public OsuClient? OsuClient { get; private set; }
 
-    public async Task RunAsync(string discordToken, string osuClientId, string osuClientSecret) {
+
+    public async Task RunAsync(string discordToken, string osuClientId, string osuClientSecret, string databaseConnectionString) {
+        var logFactory = new LoggerFactory().AddSerilog();
+        MySqlConnectorLogManager.Provider = new SerilogLoggerProvider();
+        DatabaseHandler.Configure(databaseConnectionString);
+
         OsuClient = new OsuClient(osuClientId, osuClientSecret);
 
         DiscordClient = new DiscordClient(new DiscordConfiguration {
@@ -20,7 +28,8 @@ public class DiscordBot {
             TokenType = TokenType.Bot,
             AutoReconnect = true,
             MinimumLogLevel = LogLevel.Debug,
-            Intents = DiscordIntents.AllUnprivileged
+            Intents = DiscordIntents.AllUnprivileged,
+            LoggerFactory = logFactory
         });
 
         var commands = DiscordClient.UseCommandsNext(new CommandsNextConfiguration {
@@ -29,8 +38,8 @@ public class DiscordBot {
         });
 
         commands.RegisterCommands<OsuModule>();
-        commands.RegisterCommands<MiscellaneousModule>();
         commands.RegisterCommands<TournamentModule>();
+        commands.RegisterCommands<DatabaseModule>();
 
         DiscordClient.MessageCreated += (s, messageEvent) => {
             if (messageEvent.Guild.Id == 622600219233746974) Console.WriteLine(messageEvent.Message.Content);
