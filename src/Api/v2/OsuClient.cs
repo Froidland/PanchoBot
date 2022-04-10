ef99cdb00325bd963e975458bc9c0dbb4fdd8f23
Francisco Hauva <phauva@gmail.com>
2022-04-09 23:01:47 -0400

    Now exceptions are actually thrown and handled properly. Also added logging and checking whether the request was successful or not.

--- src/Api/v2/OsuClient.cs
+++ src/Api/v2/OsuClient.cs
@@ -1,8 +1,10 @@
 ﻿#nullable enable
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 using PanchoBot.Api.v2.Models;
 using RestSharp;
+using Serilog;
 
 namespace PanchoBot.Api.v2;
 
@@ -16,43 +18,76 @@ public class OsuClient : IOsuClient {
     }
 
     public async Task<User?> GetUser(string username, string type = "username") {
-        var request = new RestRequest($"users/{username}/osu")
+        var endpoint = $"users/{username}/osu";
+        RestResponse response;
+
+        var request = new RestRequest(endpoint)
             .AddQueryParameter("key", type);
+        Log.Debug("[osu!API] Sending request to {Endpoint}", endpoint);
 
         try {
-            var response = await _client.GetAsync<User>(request);
-            return response;
+            response = await _client.GetAsync(request);
         }
-        catch {
+        catch (Exception exception) {
+            Log.Error(exception, "[osu!API] There was an error while sending a request to {Endpoint}", endpoint);
             return null;
         }
+
+        if (!response.IsSuccessful) {
+            return null;
+        }
+
+        Log.Debug("[osu!API] Request sent to {Endpoint} was successful", endpoint);
+        return JsonSerializer.Deserialize<User>(response.Content!);
     }
 
     public async Task<Beatmap?> GetBeatmap(ulong beatmapId) {
-        var request = new RestRequest($"beatmaps/{beatmapId}");
+        var endpoint = $"beatmaps/{beatmapId}";
+        RestResponse response;
+
+        var request = new RestRequest(endpoint);
+        Log.Debug("[osu!API] Sending request to {Endpoint}", endpoint);
 
         try {
-            var response = await _client.GetAsync<Beatmap>(request);
-            return response;
+            response = await _client.GetAsync(request);
         }
-        catch {
+        catch (Exception exception) {
+            Log.Error(exception, "[osu!API] There was an error while sending a request to {Endpoint}", endpoint);
             return null;
         }
+
+        if (!response.IsSuccessful) {
+            return null;
+        }
+
+        Log.Debug("[osu!API] Request sent to {Endpoint} was successful", endpoint);
+        return JsonSerializer.Deserialize<Beatmap>(response.Content!);
     }
 
     public async Task<Score[]?> GetUserScores(int userId, string type, string mode = "osu", string includeFails = "1",
         int limit = 1) {
-        var request = new RestRequest($"users/{userId}/scores/{type}")
+        var endpoint = $"users/{userId}/scores/{type}";
+        RestResponse response;
+
+        var request = new RestRequest(endpoint)
             .AddParameter("mode", mode)
             .AddParameter("includeFails", includeFails)
             .AddParameter("limit", limit);
+        Log.Debug("[osu!API] Sending request to {Endpoint}", endpoint);
 
         try {
-            var response = await _client.GetAsync<Score[]>(request);
-            return response;
+            response = await _client.GetAsync(request);
         }
-        catch (Exception e) {
+        catch (Exception exception) {
+            Log.Error(exception, "[osu!API] There was an error while sending a request to {Endpoint}", endpoint);
             return null;
         }
+
+        if (!response.IsSuccessful) {
+            return null;
+        }
+
+        Log.Debug("[osu!API] Request sent to {Endpoint} was successful", endpoint);
+        return JsonSerializer.Deserialize<Score[]>(response.Content!);
     }
 }
