84d0e62d7856a0342ca74c0a79e06d9b289f4cf5
Francisco Hauva <phauva@gmail.com>
2022-04-09 23:08:05 -0400

    The DB connection is no longer reused for the lifetime of the bot. Now each query has its own connection in order to make use of the dotnet connection pool.

--- src/Database/DatabaseHandler.cs
+++ src/Database/DatabaseHandler.cs
@@ -1,36 +1,36 @@
 ﻿using System;
+using System.Data;
 using System.Threading.Tasks;
 using MySqlConnector;
 using PanchoBot.Database.Models;
+using Serilog;
 
 namespace PanchoBot.Database;
 
 public static class DatabaseHandler {
-    private static MySqlConnection? _connection;
+    private static string _connectionString = string.Empty;
+
+    public static void Configure(string connectionString) {
+        _connectionString = connectionString;
+    }
 
     /// <summary>
     /// Connects to a database server and assigns this class' _connection variable to the connection obtained from this method.
     /// </summary>
-    /// <param name="ip">IP of the database server.</param>
-    /// <param name="username">Username to access the database server.</param>
-    /// <param name="password">Password to access the database server.</param>
-    /// <param name="database">Database name to work with in the database server.</param>
-    public static async Task ConnectAsync(string ip, string username, string password, string database) {
-        var connection = new MySqlConnection($"Server={ip};User ID={username};Password={password};Database={database}");
+    /// <param name="connectionString">Connection string for the database.</param>
+    private static async Task<MySqlConnection?> ConnectAsync(string connectionString) {
+        var connection = new MySqlConnection(connectionString);
 
         try {
-            Console.WriteLine("Establishing connection with the database.");
             await connection.OpenAsync();
-            Console.WriteLine("Database connection successful.");
         }
         catch (MySqlException e) {
-            Console.WriteLine(e.Message);
+            Log.Error("Error message: {ErrorMessage}", e.Message);
             await connection.DisposeAsync();
-            _connection = null;
-            return;
+            return null;
         }
 
-        _connection = connection;
+        return connection;
     }
 
     /// <summary>
@@ -42,11 +42,25 @@ public static class DatabaseHandler {
     /// <returns>The amount of rows affected.</returns>
     /// <exception cref="System.Data.Common.DbException">Thrown when executing the sql command fails.</exception>
     public static async Task<int> InsertUserAsync(ulong discordId, int osuId, string osuUsername) {
-        await using var cmd = new MySqlCommand(
-            $"insert into users (discord_id, osu_id, osu_username) values ({discordId}, {osuId}, '{osuUsername}')",
-            _connection);
+        string cmdText =
+            "insert into users (discord_id, osu_id, osu_username) values (@discordId, @osuId, @osuUsername)";
+        await using var connection = await ConnectAsync(_connectionString);
+        await using var cmd = new MySqlCommand(cmdText, connection);
 
-        return await cmd.ExecuteNonQueryAsync();
+        cmd.Parameters.Add("@discordId", DbType.UInt64);
+        cmd.Parameters["@discordId"].Value = discordId;
+        cmd.Parameters.Add("@osuId", DbType.Int32);
+        cmd.Parameters["@osuId"].Value = osuId;
+        cmd.Parameters.Add("@osuUsername", MySqlDbType.Text, 32);
+        cmd.Parameters["@osuUsername"].Value = osuUsername;
+
+        try {
+            return await cmd.ExecuteNonQueryAsync();
+        }
+        catch (Exception exception) {
+            Log.Error("Exception occurred: {ExceptionMessage}", exception.Message);
+            return 0;
+        }
     }
 
     /// <summary>
@@ -58,13 +72,27 @@ public static class DatabaseHandler {
     /// <returns>The amount of rows affected.</returns>
     /// <exception cref="System.Data.Common.DbException">Thrown when executing the sql command fails.</exception>
     public static async Task<int> UpdateUserAsync(ulong discordId, int osuId, string osuUsername) {
-        await using var cmd =
-            new MySqlCommand(
-                $"update users set osu_id = {osuId}, osu_username = '{osuUsername}' where discord_id = {discordId}",
-                _connection);
+        string cmdText =
+            "update users set osu_id = @osuId, osu_username = @osuUsername where discord_id = @discordId";
+        await using var connection = await ConnectAsync(_connectionString);
+        await using var cmd = new MySqlCommand(cmdText, connection);
 
-        return await cmd.ExecuteNonQueryAsync();
+        cmd.Parameters.Add("@discordId", DbType.UInt64);
+        cmd.Parameters.Add("@osuId", DbType.Int32);
+        cmd.Parameters.Add("@osuUsername", DbType.String);
+        cmd.Parameters["@discordId"].Value = discordId;
+        cmd.Parameters["@osuId"].Value = osuId;
+        cmd.Parameters["@osuUsername"].Value = osuUsername;
+
+        try {
+            return await cmd.ExecuteNonQueryAsync();
+        }
+        catch (Exception exception) {
+            Log.Error("Exception occurred: {ExceptionMessage}", exception.Message);
+            return 0;
+        }
     }
+
 
     /// <summary>
     /// Gets a row of data corresponding to the discordId in the database's "users" table.
@@ -72,9 +100,16 @@ public static class DatabaseHandler {
     /// <param name="discordId"></param>
     /// <returns>The data returned by the database if found, otherwise returns null.</returns>
     /// <exception cref="System.Data.Common.DbException">Thrown when executing the sql command fails.</exception>
-    public static async Task<DbUser?> GetUser(ulong discordId) {
-        await using var cmd = new MySqlCommand($"select * from users where discord_id = {discordId}", _connection);
+    public static async Task<DbUser?> SelectUserAsync(ulong discordId) {
+        string cmdText = "select * from users where discord_id = @discordId";
+        await using var connection = await ConnectAsync(_connectionString);
+        await using var cmd = new MySqlCommand(cmdText, connection);
+
+        cmd.Parameters.Add("@discordId", DbType.UInt64);
+        cmd.Parameters["@discordId"].Value = discordId;
+
         await using var reader = await cmd.ExecuteReaderAsync();
+
 
         if (await reader.ReadAsync()) {
             return new DbUser(reader.GetUInt64("discord_id"), reader.GetInt32("osu_id"),
@@ -82,5 +117,28 @@ public static class DatabaseHandler {
         }
 
         return null;
+    }
+
+    /// <summary>
+    /// Deletes a row of data corresponding to the discordId in the database's "users" table.
+    /// </summary>
+    /// <param name="discordId"></param>
+    /// <returns>The amount of rows affected.</returns>
+    /// <exception cref="System.Data.Common.DbException">Thrown when executing the sql command fails.</exception>
+    public static async Task<int> DeleteUserAsync(ulong discordId) {
+        string cmdText = "delete from users where discord_id = @discordId";
+        await using var connection = await ConnectAsync(_connectionString);
+        await using var cmd = new MySqlCommand(cmdText, connection);
+
+        cmd.Parameters.Add("@discordId", DbType.UInt64);
+        cmd.Parameters["@discordId"].Value = discordId;
+
+        try {
+            return await cmd.ExecuteNonQueryAsync();
+        }
+        catch (Exception exception) {
+            Log.Error("Exception occurred: {ExceptionMessage}", exception.Message);
+            return 0;
+        }
     }
 }
