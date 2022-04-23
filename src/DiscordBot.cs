1467890749acd57766099f8330fe3c06fe37df3b
Francisco Hauva <phauva@gmail.com>
2022-04-23 01:22:48 -0400

    ol

--- src/DiscordBot.cs
+++ src/DiscordBot.cs
@@ -16,7 +16,8 @@ public class DiscordBot {
     public OsuClient? OsuClient { get; private set; }
 
 
-    public async Task RunAsync(string discordToken, string osuClientId, string osuClientSecret, string databaseConnectionString) {
+    public async Task RunAsync(string discordToken, string osuClientId, string osuClientSecret,
+        string databaseConnectionString) {
         var logFactory = new LoggerFactory().AddSerilog();
         MySqlConnectorLogManager.Provider = new SerilogLoggerProvider();
         DatabaseHandler.Configure(databaseConnectionString);
@@ -41,10 +42,20 @@ public class DiscordBot {
         commands.RegisterCommands<TournamentModule>();
         commands.RegisterCommands<DatabaseModule>();
 
-        DiscordClient.MessageCreated += (s, messageEvent) => {
-            if (messageEvent.Guild.Id == 622600219233746974) Console.WriteLine(messageEvent.Message.Content);
+        DiscordClient.MessageCreated += async (s, messageEvent) => {
+            switch (messageEvent.Guild.Id) {
+                case 622600219233746974: {
+                    Console.WriteLine(messageEvent.Message.Content);
+                    break;
+                }
+                case 870106375269777418: {
+                    if (messageEvent.Channel.Id == 872487113071943711 && !messageEvent.Author.IsCurrent) {
+                        await messageEvent.Message.RespondAsync("ol");
+                    }
 
-            return null;
+                    break;
+                }
+            }
         };
 
         await DiscordClient.ConnectAsync();
