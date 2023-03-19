139c9ef908264ace30517c48832948a2683c8e05
Francisco Hauva <phauva@gmail.com>
2023-03-19 03:42:20 -0300

    Template for commands cause I'm a little dumb dumb.

---
+++ src/commands/template.ts
@@ -0,0 +1,9 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../interfaces/command";
+
+export const template: Command = {
+  data: new SlashCommandBuilder()
+    .setName("Template")
+    .setDescription("Template"),
+  execute: async (interaction) => {},
+};
