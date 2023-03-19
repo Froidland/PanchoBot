e8e146e72fcbc9d90d57fdcb450b376af109c4f8
Francisco Hauva <phauva@gmail.com>
2023-03-18 22:38:30 -0300

    Moved Archive category command to utility folder.

--- src/commands/_commandList.ts
+++ src/commands/_commandList.ts
@@ -1,6 +1,12 @@
 import { Command } from "../interfaces/command";
 import { ping } from "./general";
 import { link, profile, unlink } from "./osu";
-import { archiveCategory } from "./tournament";
+import { archiveCategory } from "./utility";
 
-export const commandList: Command[] = [ping, profile, link, unlink, archiveCategory];
+export const commandList: Command[] = [
+  ping,
+  profile,
+  link,
+  unlink,
+  archiveCategory,
+];
