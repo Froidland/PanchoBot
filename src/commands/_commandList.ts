4fb25d8371969838284406f13cfaeee296cdc2b9
Francisco Hauva <phauva@gmail.com>
2023-05-22 14:07:55 -0400

    Add emoji stealing command

--- src/commands/_commandList.ts
+++ src/commands/_commandList.ts
@@ -2,13 +2,14 @@ import { Command } from "../interfaces/command";
 import { ping } from "./general";
 import { link, profile, unlink } from "./osu";
 import { createTournament } from "./tournament";
-import { archiveCategory } from "./utility";
+import { archiveCategory, stealEmoji } from "./utility";
 
 export const commandList: Command[] = [
-  ping,
-  profile,
-  link,
-  unlink,
-  archiveCategory,
-  createTournament,
+	ping,
+	profile,
+	link,
+	unlink,
+	archiveCategory,
+	createTournament,
+	stealEmoji
 ];
