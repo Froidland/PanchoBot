29aa83805ddb7ab4a77c4086a085efe2a02f16f7
Francisco Hauva <phauva@gmail.com>
2023-09-22 20:51:05 -0300

    fix: handle interactions of type ContextMenuCommand

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,11 +1,21 @@
 import { Interaction } from "discord.js";
-import { slashCommandList } from "../handlers";
+import { contextMenuCommandList, slashCommandList } from "../handlers";
 
 export const onInteraction = async (interaction: Interaction) => {
 	if (interaction.isCommand()) {
 		for (const command of slashCommandList) {
 			if (interaction.commandName === command.data.name) {
 				await command.execute(interaction);
+				break;
+			}
+		}
+	}
+
+	if (interaction.isContextMenuCommand()) {
+		for (const contextCommand of contextMenuCommandList) {
+			if (interaction.commandName === contextCommand.data.name) {
+				await contextCommand.execute(interaction);
+
 				break;
 			}
 		}
