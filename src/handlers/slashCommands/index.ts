3f96e95b60ae9451d5ecf91f2e62cc4ebd36f6e9
Francisco Hauva <phauva@gmail.com>
2023-09-22 20:44:11 -0300

    refactor: remove unnecessary exports

--- src/handlers/slashCommands/index.ts
+++ src/handlers/slashCommands/index.ts
@@ -2,7 +2,4 @@ import { Command } from "../../interfaces/command";
 import { archiveCategory } from "./archiveCategory";
 import { addEmoji } from "./addEmoji";
 
-export * from "./archiveCategory";
-export * from "./addEmoji";
-
-export const slashCommandList: Command[] = [archiveCategory, addEmoji]
+export const slashCommandList: Command[] = [archiveCategory, addEmoji];
