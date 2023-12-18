525003854d8274818a106c90a71f4e0a73be9f28
Francisco Hauva <phauva@gmail.com>
2023-12-18 14:56:45 -0300

    feat: remove message translation

--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -1,12 +1,5 @@
 import { Message } from "discord.js";
 
-type Translation = {
-	translations: {
-		detected_source_language: string;
-		text: string;
-	}[];
-};
-
 export const onMessageCreate = async (message: Message) => {
 	if (!message.author.bot) {
 		switch (message.channel.id) {
@@ -32,53 +25,6 @@ export const onMessageCreate = async (message: Message) => {
 			case "968359767493967883":
 				await message.delete();
 				break;
-			case "1104239309806125158":
-				if (!translationWhitelist.includes(message.author.id)) {
-					return;
-				}
-
-				const translation = await getTranslation(message.content);
-
-				if (translation) {
-					await message.reply({
-						content: translation,
-						allowedMentions: { repliedUser: false },
-					});
-				}
-
-				break;
 		}
 	}
 };
-
-async function getTranslation(message: string) {
-	const apiUrl = "https://api-free.deepl.com/v2/translate";
-
-	const translationResponse = await fetch(
-		`${apiUrl}?text=${encodeURIComponent(message)}&target_lang=EN`,
-		{
-			method: "POST",
-			headers: {
-				Authorization: "DeepL-Auth-Key " + process.env.DEEPL_API_KEY,
-			},
-		}
-	);
-
-	if (!translationResponse.ok) {
-		return null;
-	}
-
-	const translation = (await translationResponse.json()) as Translation;
-
-	if (translation.translations[0].detected_source_language === "EN") {
-		return null;
-	}
-
-	return translation.translations[0].text;
-}
-
-const translationWhitelist = [
-	"329379151364751360",
-	"292478478295498762",
-	"278368808627077120",
-];
